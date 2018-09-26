a78a486376c5edb856f6e34db4100d61e27644dc
HarutAntonyan778 <antonyanharut@icloud.com>
2018-09-26 20:37:45 +0400

    Checked homework

--- homeworks/Anush_Mkrtchyan/zujgkam kent.java
+++ homeworks/Anush_Mkrtchyan/zujgkam kent.java
@@ -1,8 +1,16 @@
+/*
+File name and class name doesn't much with each other. 
+Also file name can't start with lower case letters and can't contain any spaces
+*/
+
 public class Main {
 
     public static void main(String[] args) {
-	Scanner scanner=new Scanner(Syste.in);
+	Scanner scanner=new Scanner(Syste.in); // What is Syste class?
 	int n=input.nextInt();
-	System.out.println((n%2)*(x*x);
+	System.out.println((n%2)*(x*x); // be correct , where is ')' ?
     }
 }
+
+			   
+			 
