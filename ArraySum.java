2afd23b9a5f035ccdbd06b6bb67c0f7a2a9635ea
Dinunts <dinuntsas@gmail.com>
2018-09-27 20:01:53 +0400

    Updated homework

---
+++ ArraySum.java
@@ -0,0 +1,15 @@
+public class ArraySum {
+        public static void main(String args[]){
+int [] array1={1,2,3,4,5};
+int [] array2={6,7,8,9,10};
+int [] array3=new int[5];
+
+for (int i = 0; i < array3.length; i++)  {
+                        array3 [i]=array1[i]+array2[i];
+
+                        
+System.out.println(array3[i]);
+
+}
+}
+}
