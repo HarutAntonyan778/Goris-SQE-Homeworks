76800ebf9f51bb18aef99aca7b34b90070cabbb0
ArsenDadunts <arsen.dadunts.itc@gmail.com>
2018-09-28 21:23:54 +0400

    Add the Bubble sort homework.

---
+++ homeworks/Arsen_Dadunts/BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort {  
+public static void main (String [] args){  
+int [] arr={9,8,5,1,2,74,35,2,33,58,92};
+int n = arr.length;  
+int temp = 0;  
+for(int i=0; i < n; i++){  
+for(int j=1; j < (n-i); j++){  
+if(arr[j-1] > arr[j]){  
+temp = arr[j-1];  
+arr[j-1] = arr[j];  
+arr[j] = temp;  
+   }  
+  }
+ }
+for (int i=0; i<arr.length; i++){
+System.out.println(arr[i]);
+  }  
+ } 
+}
