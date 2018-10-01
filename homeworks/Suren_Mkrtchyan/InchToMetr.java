31fe9ac54f63a0b64a51b626b4026d768942ecaf
[Suren <[suren.mkrtchyan.itc@gmail.com]>
2018-10-01 23:44:12 +0400

    Changed homeworks

--- homeworks/Suren_Mkrtchyan/InchToMetr.java
+++ homeworks/Suren_Mkrtchyan/InchToMetr.java
@@ -1,6 +1,6 @@
-public class MyClass {
+public class InchToMetr {
     public static void main(String args[]) {
-        int m=54;
+        int m=55;
         double i=m*0.0254;
         System.out.println(i);
     }
