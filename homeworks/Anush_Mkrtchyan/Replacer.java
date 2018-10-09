972be3173c9b10f1a8f2aa5a27bbac7defd78951
AnushMkrtchyan888 <anush.mkrtchyan.itc@gmail.com>
2018-10-09 16:37:21 +0400

    It is Replace method

---
+++ homeworks/Anush_Mkrtchyan/Replacer.java
@@ -0,0 +1,25 @@
+public class Replacer {
+
+    public static void main(String[] args) {
+        replacel("Armenia England French Engfort ", "Eng", "Ru");
+    }
+
+    public static void replacel (String text, String oldw, String neww) {
+        for (int i = 0; i < text.length(); ++i) {
+            int temp = 0;
+            if (text.charAt(i) == oldw.charAt(0)) {
+                for (int j = 1; j < oldw.length(); ++j) {
+                    if (text.charAt(i + j) == oldw.charAt(j)) {
+                        temp++;
+                    }
+                }
+            }
+            if (temp == (oldw.length() - 1))
+                text = text.substring(0, i) + neww + text.substring(i + oldw.length());
+        }
+
+        System.out.println(text);
+    }
+}
+
+
