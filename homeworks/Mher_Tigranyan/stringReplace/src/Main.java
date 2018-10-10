3c5d12bd7b137463711d78b7ce46da8b94848783
MherTigranyan <mher.tigranyan.itc@gmail.com>
2018-10-10 16:48:49 +0400

    any changes in stringReplace folder

--- homeworks/Mher_Tigranyan/stringReplace/src/Main.java
+++ homeworks/Mher_Tigranyan/stringReplace/src/Main.java
@@ -4,13 +4,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args)
     {
-        String originalString = "This car is my car";
-        String replacedString = replaceMethod(originalString, "car", "bike");
+        originalString = "This car is my car";
+        replacedString = replaceMethod(originalString, "car", "bike");
         System.out.println(replacedString);
     }
     static int leng;
-    static String replaceMethod(String str, String from, String to)
-    {
+    static String originalString;
+    static String replacedString;
+    static int pivot1;
+    static int pivot2;
+    static String from;
+    static String to;
+    static String replaceMethod(String str, String from, String to) {
         for (int i = 0; i<str.length(); i++){
             for (int j = 0; j<from.length(); j++){
                 if (str.charAt(i) == from.charAt(j)){
@@ -19,25 +24,32 @@ public class Main {
                 }
                 if (j >= from.length()){
                     leng= i-from.length()+to.length()+str.length()-i;
-                    str = changeMethod(str, i-from.length(), i);
+                    pivot1=i-from.length();
+                    pivot2=i;
+                    str = changeMethod();
                 }
             }
         }
         return str;
     }
-    static String changeMethod(String rString, int p1, int p2){
+    static String changeMethod(){
 
         char[] ch = new char[leng];
         for (int i=0; i< leng; i++){
-            if (i<=p1) {
-                ch[i] = rString.charAt(i);
-            } else if (i>p1 && i<p2){
-                for (int k = 0; k<p2-p1; k++){
-                    ch[i] =  
+            if (i<=pivot1) {
+                ch[i] = originalString.charAt(i);
+            } else if (i>pivot1 && i<pivot2){
+                for (int k = 0; k<to.length(); k++) {
+                    ch[i] = to.charAt(k);
+                    i++;
                 }
+              i=i-to.length()+from.length();
+            }  else {
+                ch[i] = originalString.charAt(i-to.length()-from.length());
             }
         }
-        return rString;
+        replacedString = ch.toString();
+        return replacedString;
     }
 
 }
