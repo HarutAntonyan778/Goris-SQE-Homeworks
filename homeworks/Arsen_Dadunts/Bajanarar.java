107d18c674388ab3277ece22cc80a16678bed9e3
ArsenDadunts <arsen.dadunts.itc@gmail.com>
2018-10-17 13:19:05 +0400

    Added the Bajanarar homework

---
+++ homeworks/Arsen_Dadunts/Bajanarar.java
@@ -0,0 +1,18 @@
+public class Bajanarar {
+    public static void main(String[] args) {
+        int a = 5245;
+        int b = 65;
+        if (a == 0 || b == 0) System.out.println("Chuni @nhdanur bajanarar");
+        else {
+            while (a != 0 && b != 0) {
+                if (a >= b) {
+                    a = a % b;
+                } else {
+                    b = b % a;
+                }
+            }
+            if (a == 0) System.out.println(b);
+            else System.out.println(a);
+        }
+    }
+}
