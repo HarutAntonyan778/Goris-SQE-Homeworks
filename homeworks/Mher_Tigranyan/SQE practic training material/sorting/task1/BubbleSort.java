493216ff79c66ca0aeab546a17e321f68abb9282
MherTigranyan <mher.tigranyan.itc@gmail.com>
2019-01-06 00:33:26 +0400

    add Task1 of sort

---
+++ homeworks/Mher_Tigranyan/SQE practic training material/sorting/task1/BubbleSort.java
@@ -0,0 +1,27 @@
+public class BubbleSort {
+    private static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+    }
+    private static void printArray(int[] arr) {
+        for (int anArr : arr) {
+            System.out.print(anArr + " ");
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {16, 3, 5, 1, 13, 9, 7, 4};
+        printArray(arr);
+        bubbleSort(arr);
+        printArray(arr);
+    }
+}
---
+++ homeworks/Mher_Tigranyan/SQE practic training material/sorting/task1/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 9, 5, 3, 4, 3};
+        counting_sort(arr1);
+    }
+     static void counting_sort(int[] arr1){
+        int temp = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            if (temp < arr1[i]){
+                temp = arr1[i];
+            }
+        }
+        int[] arr2 = new int[temp + 1];
+
+         for (int i = 0; i < arr1.length; i++) {
+             arr2[arr1[i]] += 1;
+         }
+
+         for (int i = 0; i < arr2.length; i++) {
+             while (arr2[i] != 0){
+                 System.out.print(i + ", ");
+                 arr2[i] -= 1;
+             }
+         }
+    }
+}
