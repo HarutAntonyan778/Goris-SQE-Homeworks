68a67bd87cf2463d0d54b026899fde3c5ff01984
ArsenDadunts <40470937+ArsenDadunts@users.noreply.github.com>
2019-01-24 15:54:39 +0400

    Update Main.java

--- homeworks/Arsen_Dadunts/CountingSort/src/Main.java
+++ homeworks/Arsen_Dadunts/CountingSort/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         CountingSort countingSort=new CountingSort();
-        int array1={4,3,2,7,98,65,43,21,1,2,0,54};
-        int array2={5,32,23,13,38,63,4,13,41,23,10,52};
-        int array3={2,25,21,33,28,35,15,43,13,13,178,152};
-
-        countingSort.sort();
+        int [] array1={4,3,2,7,98,65,43,21,1,2,0,54};
+        int [] array2={5,32,23,13,38,63,4,13,41,23,10,52};
+        int [] array3={2,25,21,33,28,35,15,43,13,13,178,152};
+        countingSort.sort(array1);
+        countingSort.sort(array2);
+        countingSort.sort(array3);
     }
 }
-
