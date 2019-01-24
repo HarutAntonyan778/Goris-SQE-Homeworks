ec4c4e08786333d1a4285b3bedfada8775f7c54d
ArsenDadunts <40470937+ArsenDadunts@users.noreply.github.com>
2019-01-24 17:32:31 +0400

    Update Main.java

--- homeworks/Arsen_Dadunts/Search/Task3/src/Main.java
+++ homeworks/Arsen_Dadunts/Search/Task3/src/Main.java
@@ -1,41 +1,37 @@
-import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
-            findsmallestnumbers();
-        }
-        public static void findsmallestnumbers() {
-            Scanner scanner =new Scanner(System.in);
-            System.out.print("Enter the array length: ");
-            int length=scanner.nextInt();
-            int [] a = new int[length];
-            int temp=0;
-            while (temp<length){
-                a[temp]=scanner.nextInt();
-                temp++;
-            }
-            if (a.length<2){
-                System.out.println("The array have not enough element:");
-                findsmallestnumbers();
-            }
-            if (a.length==2){
-               if (a[0]>a[1]) System.out.println("The first smallest number is: " + a[1]);
-               else System.out.println("The first smallest number is: " + a[0]);
-                System.out.println("The array have not second smallest element:");
-                System.exit(0);
-            }
-            int min1 = a[1];
-            int min2 = a[2];
-
-            for (int i = 0; i <a.length; i++) {
-                if (min1>a[i]) {
-                    min2 = min1;
-                    min1 = a[i];
-                }
-                else if (a[i] < min2 && a[i] != min1)
-                    min2 = a[i];
-            }
-
-            System.out.println("The first smallest number is"  + min1 +"\nThe second smallest number is: "+ min2);
-        }
+    public static void main(String[] args) {
+        int [] array1={4,3,2,7,98,65,43,21,1,2,0,54};
+        int [] array2={5,32,23,13,38,63,4,13,41,23,10,52};
+        int [] array3={2,25,21,33,28,35,15,43,13,13,178,152};
+        findsmallestnumbers(array1);
+        findsmallestnumbers(array2);
+        findsmallestnumbers(array3);
     }
+    public static void findsmallestnumbers(int arr []) {
+
+        if (arr.length<2){
+            System.out.println("The array have not enough element:");
+            return;
+        }
+        if (arr.length==2){
+            if (arr[0]>arr[1]) System.out.println("The first smallest number is: " + arr[1]);
+            else System.out.println("The first smallest number is: " + arr[0]);
+            System.out.println("The array have not second smallest element:");
+            System.exit(0);
+        }
+        int min1 = arr[1];
+        int min2 = arr[2];
+
+        for (int i = 0; i <arr.length; i++) {
+            if (min1>arr[i]) {
+                min2 = min1;
+                min1 = arr[i];
+            }
+            else if (arr[i] < min2 && arr[i] != min1)
+                min2 = arr[i];
+        }
+
+        System.out.println("The first smallest number is: "  + min1 +"\t The second smallest number is: "+ min2);
+    }
+}
