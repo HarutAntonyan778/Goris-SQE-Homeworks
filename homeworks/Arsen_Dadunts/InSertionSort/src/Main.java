9d89d778cbe7e5f1831b035bf54e640a3d738bec
ArsenDadunts <40470937+ArsenDadunts@users.noreply.github.com>
2019-01-24 16:04:21 +0400

    Update Main.java

--- homeworks/Arsen_Dadunts/InSertionSort/src/Main.java
+++ homeworks/Arsen_Dadunts/InSertionSort/src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         InSertion inSertion =new InSertion();
-        int a [] ={15,26,7,8,6,4,2,6,3,4,5,1,9,12,10};
-        inSertion.sort(a);
+        int array [] ={15,26,7,8,6,4,2,6,3,4,5,1,9,12,10};
+        int [] array1={4,3,2,7,98,65,43,21,1,2,0,54};
+        int [] array2={5,32,23,13,38,63,4,13,41,23,10,52};
+        int [] array3={2,25,21,33,28,35,15,43,13,13,178,152};
+        inSertion.sort(array);
+        inSertion.sort(array1);
+        inSertion.sort(array2);
+        inSertion.sort(array3);
     }
 }
