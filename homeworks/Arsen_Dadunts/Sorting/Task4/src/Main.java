79560a5cebc4020b78cac9e45623a40f1fff651f
ArsenDadunts <40470937+ArsenDadunts@users.noreply.github.com>
2019-01-24 17:47:33 +0400

    Update Main.java

--- homeworks/Arsen_Dadunts/Sorting/Task4/src/Main.java
+++ homeworks/Arsen_Dadunts/Sorting/Task4/src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int a []= {10, 5, 6, 3, 2, 20, 100, 80};
+        int [] array1={4,3,2,7,98,65,43,21,1,2,0,54};
+        int [] array2={5,32,23,13,38,63,4,13,41,23,10,52};
+        int [] array3={2,25,21,33,28,35,15,43,13,13,178,152};
         wavelikearray(a);
+        wavelikearray(array1);
+        wavelikearray(array2);
+        wavelikearray(array3);
     }
     public static void wavelikearray(int arr[]){
         for (int i = 0; i <arr.length-2; i++) {
@@ -14,5 +20,6 @@ public class Main {
         for (int output:arr) {
             System.out.print(" "+output);
         }
+        System.out.println();
     }
 }
