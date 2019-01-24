c45fb73d2bea14fd303d42922a0d2e7555bba823
ArsenDadunts <40470937+ArsenDadunts@users.noreply.github.com>
2019-01-24 17:52:53 +0400

    Update Main.java

--- homeworks/Arsen_Dadunts/Sorting/Task5/src/Main.java
+++ homeworks/Arsen_Dadunts/Sorting/Task5/src/Main.java
@@ -1,20 +1,19 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int arr[] = {1, 14, 11, 51, 15};
-        printmissingelements(arr);
+        int [] array= {1, 14, 11, 51, 15};
+        int [] array1={4,3,2,7,98,65,43,21,1,2,0,54};
+        int [] array2={5,32,23,13,38,63,4,13,41,23,10,52};
+        int [] array3={2,25,21,33,28,35,15,43,13,13,178,152};
+        printmissingelements(array, 10, 15);
+        printmissingelements(array1, 20,30);
+        printmissingelements(array2, 15,19);
+        printmissingelements(array3, 35,45);
 
     }
 
-    public static void printmissingelements(int arr[]) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the low number:");
-        int low = scanner.nextInt();
-        System.out.println("Enter the hight number:");
-        int hight = scanner.nextInt();
-        if (low > hight) printmissingelements(arr);
+    public static void printmissingelements(int arr[], int low, int hight) {
+
+        if (low > hight) return;
 
         int arr2[] = new int[hight - low];
         boolean list[] = new boolean[hight - low];
@@ -31,7 +30,8 @@ public class Main {
         }
 
         for (int i = 0; i < list.length; i++) {
-            if (list[i] == true)System.out.println(" " + arr2[i]);
+            if (list[i] == true)System.out.print(" " + arr2[i]);
         }
+        System.out.println();
     }
 }
